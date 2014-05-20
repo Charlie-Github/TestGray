464a540184a3c68f96d5e5c9cf7ebc20a685aedb
Charlie Gao <gcte2010@gmail.com>
2014-05-20 15:38:11 -0500

    Initial Commit

---
+++ TestGray/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestGray
+//
+//  Created by CharlieGao on 5/20/14.
+//  Copyright (c) 2014 Edible Innovations. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
